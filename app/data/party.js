cb317a6ccd65b84ed35eb2fca5e48a1c79f0b101
Xerdet <lurobper@gmail.com>
2019-03-05 15:27:20 -0600

    Working code uploaded. CSS working correctly for Heroku

--- app/data/party.js
+++ app/data/party.js
@@ -2,43 +2,43 @@ var availableParty = [
     {
         name: "Osmulf",
         class: "Fighter",
-        photo: "../public/Images/Osmulf.png",
+        photo: "Osmulf.png",
         scores: [5, 1, 3, 1, 4, 1, 1, 5, 2, 2]
     },
     {
         name: "Maglignor",
         class: "Rogue",
-        photo: "../public/Images/Maglignor.png",
+        photo: "Maglignor.png",
         scores: [1,3,5,5,3,1,5,2,5,1]
     },
     {
         name: "Lemminar",
         class: "Cleric",
-        photo: "../public/Images/Lemminar.png",
+        photo: "Lemminar.png",
         scores: [2,4,1,5,5,5,2,1,4,1]
     },
     {
         name: "Gylas",
         class: "Paladin",
-        photo: "../public/Images/Gylas.png",
+        photo: "Gylas.png",
         scores: [5,3,1,1,5,4,1,4,1,1]
     }, 
     {
         name: "Sabeth",
         class: "Wizard",
-        photo: "../public/Images/Sabeth.png",
+        photo: "Sabeth.png",
         scores: [1,5,2,5,5,3,3,1,4,3]
     },
     {
         name: "Sabyl",
         class: "Bard",
-        photo: "../public/Images/Sabyl.png",
+        photo: "Sabyl.png",
         scores: [2,5,5,3,5,3,4,1,3,5]
     },
     {
         name: "Xerdet",
         class: "Alchemist",
-        photo: "../public/Images/Xerdet.png",
+        photo: "Xerdet.png",
         scores: [2,3,4,4,5,3,3,1,5,3]
     }
 ];
